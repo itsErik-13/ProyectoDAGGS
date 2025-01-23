b748b6a136a66ebc0eff95b83ae2bd0dd34d8427
itsErik-13 <erik.p.f.2001@gmail.com>
2025-01-23 16:49:56 +0100

    Format

--- src/main/java/es/uvigo/dagss/recetas/entidades/Receta.java
+++ src/main/java/es/uvigo/dagss/recetas/entidades/Receta.java
@@ -17,20 +17,8 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 public class Receta  implements Serializable{
-    /*
-     * prescripción a la que pertenece la receta
-     * fecha de validez inicial, a partir de la cúal el paciente puede pasar por una
-     * farmacia a recojer "cajas" del medicamento
-     * fecha de validez final, pasada la cuál no será posible recoger "cajas" del
-     * medicamento
-     * número de unidades del medicamento (= "cajas") a servir por parte de la
-     * farmacia donde se presente la receta
-     * estado de la receta [PLANIFICADA, SERVIDA, ANULADA], inicialmente con valor
-     * PLANIFICADA
-     * farmacia que sirvió la receta, inicialmente tendrá valor NULL (se establecerá
-     * la relación cuando se se haga efectiva la entrega de las "cajas")
-     */
-     @Id
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
